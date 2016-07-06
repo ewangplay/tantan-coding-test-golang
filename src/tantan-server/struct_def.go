89975c875c5752f44f6476b550516c50b3f73c17
ewangplay <ewangplay@gmail.com>
2016-07-06 22:32:43 +0800

    imple users api

--- src/tantan-server/struct_def.go
+++ src/tantan-server/struct_def.go
@@ -1,12 +1,12 @@
 package main
 
-type User struct {
+type TT_User struct {
 	Id   string `json:"id"`
 	Name string `json:"name"`
 	Type string `json:"type"`
 }
 
-type Relationship struct {
+type TT_Relationship struct {
 	User_id string `json:"user_id"`
 	State   string `json:"state"`
 	Type    string `json:"type"`
--- src/tantan-server/user_handler.go
+++ src/tantan-server/user_handler.go
@@ -4,6 +4,15 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+    "encoding/json"
+    "io/ioutil"
+)
+
+var g_userIdSeed int64
+
+const (
+	GET_ALL_USERS_SQL string = `SELECT * from user_tbl`
+    ADD_USER_SQL string = `INSERT INTO user_tbl(id, name, type) VALUES(?,?,?)`
 )
 
 func UsersHandler(w http.ResponseWriter, r *http.Request) {
@@ -27,9 +36,65 @@ func UsersHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func ListAllUsers() (result string, err error) {
-	return "", nil
+
+	var users []TT_User
+
+	err = g_pgAdaptor.Query(&users, GET_ALL_USERS_SQL)
+	if err != nil {
+		return "", err
+	}
+
+	r, err := json.Marshal(users)
+	if err != nil {
+		return "", err
+	}
+
+	return string(r), nil
 }
 
 func AddUser(r *http.Request) (result string, err error) {
-	return "", nil
+    //read the body data
+    body, err := ioutil.ReadAll(r.Body)
+	if err != nil && err != io.EOF {
+		return "", err
+	}
+
+    //parse the body data
+	var user TT_User
+    user.Type = "user"
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		return "", err
+	}
+
+    //check the data field
+    if user.Name == "" {
+        return "", fmt.Errorf("user name empty!")
+    }
+
+    //increment user id
+    user.Id, err = GenerateUserID()
+    if err != nil {
+        return "", err
+    }
+
+    //insert data into db
+    err = g_pgAdaptor.Exec(ADD_USER_SQL, user.Id, user.Name, user.Type)
+    if err != nil {
+        return "", err 
+    }
+
+	rb, err := json.Marshal(&user)
+	if err != nil {
+		return "", err
+	}
+
+	return string(rb), nil
+}
+
+func GenerateUserID() (id string, err error) {
+    //This just a temporay solution.
+    //In fact, here need to lock， and need to save last value when server exit
+    g_userIdSeed++
+    return fmt.Sprintf("%v", g_userIdSeed), nil
 }
